42104d4de7f3542216d870cfd41b4eea02b9c13c
jonasdevzero <jonasdevzero@gmail.com>
2022-01-01 12:52:30 -0300

    fix

--- vanilla-api/src/routes.js
+++ vanilla-api/src/routes.js
@@ -5,12 +5,12 @@ const router = new Router()
 
 const userController = UserFactory.generateInstance()
 
-router.get("/", (req, res) => res.status(200).text("Ok"))
+router.get("/", (_req, res) => res.status(200).text("Ok"))
 
 // Wildcard test route
 router.get("/wildcard/*", (req, res) => {
     const path = req.params["*"]
-    res.status(200).send({ message: `Selected path /test/${path}` })
+    res.status(200).send({ message: `Selected path /wildcard/${path}` })
 })
 
 // User routes
